546e61778c28d75e8920f1b6787b763b6999a9fc
Ravindu156 <puresoftlk@gmail.com>
2023-12-14 22:31:49 +0530

    completion

--- phpMySql.php
+++ phpMySql.php
@@ -8,14 +8,30 @@ $dbname="myDB";
 //Create connection
 $conn=new mysqli($servername,$username,$password,$dbname);
 
-//
+//check connection
+if($conn->connect_error){
+    die("Connection failed: ".$conn->connect_error);
+}
+
+//sql to craete table 
+
+$sql="CREATE TABLE MyGuests(
+    id INT(6) AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+
+)";
+
+if($conn->query($sql)==TRUE){
+    echo "Table MyGuests created successfully";
+}else{
+    echo "Error creating table: ".$conn->error;
+}
 
 
-
-
-
-
-
+$conn->close();
 
 
 
